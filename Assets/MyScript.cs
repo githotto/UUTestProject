92b94a7a197fd3134d9445e95962f9f5fbbc5268
Otto van Verseveld <ohvanverseveld@gmail.com>
2016-05-15 15:02:28 +0200

    Added a script.

---
+++ Assets/MyScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("You found the hidden gem");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
